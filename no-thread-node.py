2e9783b17c4bb90b5604625a836a25911d94fce8
francesco <francesco.gadaleta@gmail.com>
2018-07-31 18:07:43 +0200

    linted

--- no-thread-node.py
+++ no-thread-node.py
@@ -1,61 +1,60 @@
-import random
+# import random
 import os
 import networkx as nx
-#import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import logging
-import asyncio
-import threading
-import datetime
-import signal
-import sys
+# import asyncio
+# import threading
+# import datetime
+# import signal
+# import sys
 import time
 import queue
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
-
-    
 MIN_NUM_SIG = 4
+
 
 def create_transaction(nonce, value=None, signed_by=None):
     if not signed_by:
         signatures = []
-    
+
     transaction = {'key': os.urandom(32).hex(),
                    'value': value,
                    'signed_by': signatures,
                    'nonce': nonce,
                    'prev_tx': '',
-    }
+                   }
     return transaction
 
 
 class Node():
     def __init__(self, label=None):
-        self.storage    = {}
-        self.neighbors  = []
-        self.queue      = []
-        self.node_id    = os.urandom(32).hex()
+        self.storage = {}
+        self.neighbors = []
+        self.queue = []
+        self.node_id = os.urandom(32).hex()
         self.node_label = label
-        self.nonce      = 0
-        self.tx_queue   = queue.Queue()
-        self.tx_final   = []
-        self.promise    = 0  # next sequence to validate 
+        self.nonce = 0
+        self.tx_queue = queue.Queue()
+        self.tx_final = []
+        self.promise = 0  # next sequence to validate
 
     def __repr__(self):
         return str(self.node_label)
-                
-    def connect(self, neighs:list):
+
+    def connect(self, neighs: list):
         self.neighbors = neighs
 
     def sign(self, tx):
-        # if havent signed yet, sign 
-        k,v,s,n = tx['key'], tx['value'], tx['signed_by'], tx['nonce']
-        
+        # if havent signed yet, sign
+        k, v, s, n = tx['key'], tx['value'], tx['signed_by'], tx['nonce']
+
         if self.node_id not in tx['signed_by']:
             s.append(self.node_id)
-            
-        return {'key': k, 'value': v, 'signed_by': s, 'nonce':n}
+
+        return {'key': k, 'value': v, 'signed_by': s, 'nonce': n}
 
     def broadcast(self, tx):
         # send tx to neighbors
@@ -63,21 +62,20 @@ class Node():
             time.sleep(0.01)
             log.debug('node %s broadcasting to %s', self.node_label, n)
             n.tx_queue.put(tx)
-            
+
     def propose(self, value=None):
         self.nonce += 1
         tx = create_transaction(nonce=self.nonce, value=value)
         signed_tx = self.sign(tx)
-        k,v,s,n = signed_tx['key'], signed_tx['value'], signed_tx['signed_by'], signed_tx['nonce']
-        self._store(k,v,s,n)
-        
+        k, v, s, n = signed_tx['key'], signed_tx['value'], signed_tx['signed_by'], signed_tx['nonce']
+        self._store(k, v, s, n)
+
         for n in self.neighbors:
             n.tx_queue.put(signed_tx)
 
     def _store(self, key, value, sigs, nonce):
         self.storage[key] = {'value': value, 'signed_by': sigs, 'nonce': nonce}
 
-    
     def run(self):
         log.debug('processing queue in node %s', self.node_label)
         while not self.tx_queue.empty():
@@ -85,79 +83,70 @@ class Node():
             tx = self.tx_queue.get()
             log.debug('node %s reading queue tx=%s', self.node_label, tx)
             # extract key, value, signers
-            k,v,s,n = tx['key'], tx['value'], tx['signed_by'], tx['nonce']
-            tx_score = len(s) # transaction score == num of signatures
-
-            
-            
+            k, v, s, n = tx['key'], tx['value'], tx['signed_by'], tx['nonce']
+            tx_score = len(s)  # transaction score == num of signatures
             # this is a final transaction
             # WIP
             """
             if tx_score >= MIN_NUM_SIG:
                 signed_tx = self.sign(tx)
-                # promise to accept only the next 
+                # promise to accept only the next
                 self.promise = n+1
                 self._store(k,v,s,n)
                 continue
             """
-                
-                
-            
-            
             # tx is not in storage: sign and broadcast
             if k not in self.storage:
                 signed_tx = self.sign(tx)
                 # store signed transaction
-                k,v,s,n = signed_tx['key'], signed_tx['value'], signed_tx['signed_by'], signed_tx['nonce']
+                k, v, s, n = signed_tx['key'], signed_tx['value'], signed_tx['signed_by'], signed_tx['nonce']
                 tx_score = len(s)
-                self._store(k,v,s,n)
-                #self.storage[k] = {'value': v, 'signed_by': s}
+                self._store(k, v, s, n)
+                # self.storage[k] = {'value': v, 'signed_by': s}
                 time.sleep(.001)
                 log.debug('node %s new transaction storing tx %s score=%s',
                           self.node_label, tx['key'], tx_score)
                 # broadcast to network
                 self.broadcast(signed_tx)
-                
-            # tx is already in local storage 
+
+            # tx is already in local storage
             if k in self.storage:
                 local_score = len(self.storage[k]['signed_by'])
-                log.debug('node %s existing transaction %s', self.node_label,tx['key'])
-                log.debug('\t local score = %s, new score = %s', local_score, tx_score) 
-                # accept only if better 
+                log.debug('node %s existing transaction %s', self.node_label, tx['key'])
+                log.debug('\t local score = %s, new score = %s', local_score, tx_score)
+                # accept only if better
                 if tx_score > local_score:
                     log.debug('node %s better transaction tx %s', self.node_label, tx['key'])
-                    #self.storage[k] = {'value': v, 'signed_by': s}
-                    self._store(k,v,s,n)
+                    # self.storage[k] = {'value': v, 'signed_by': s}
+                    self._store(k, v, s, n)
                     time.sleep(.001)
-                
+
                     # broadcast updated tx
-                    new_tx = {'key': k, 'value': v, 'signed_by': s, 'nonce':n}
+                    new_tx = {'key': k, 'value': v, 'signed_by': s, 'nonce': n}
                     signed_tx = self.sign(new_tx)
                     self.broadcast(signed_tx)
+            # self.tx_queue.task_done()
 
-            #self.tx_queue.task_done()
 
-                
-        
 if __name__ == '__main__':
-    num_nodes = 100
-    min_score = 4
-    num_transactions = 30
+    NUM_NODES = 10
+    MIN_SCORE = 4
+    NUM_TXS = 30
     # TODO
     # transaction finality
-    # leader election 
-    
+    # leader election
+
     # setup network
     nodes = []
-    for k, i in enumerate(range(num_nodes)):
+    for k, i in enumerate(range(NUM_NODES)):
         n = Node(label=k)
         nodes.append(n)
-    network = nx.watts_strogatz_graph(num_nodes, 2, 0.1)
-    #nx.draw(network, with_labels=True, font_weight='bold')
-    #plt.show()
+    network = nx.watts_strogatz_graph(NUM_NODES, 2, 0.1)
+    # nx.draw(network, with_labels=True, font_weight='bold')
+    # plt.show()
 
     # connect nodes as in graph
-    for i in range(num_nodes):
+    for i in range(NUM_NODES):
         neigh_i = list(network[i].keys())
         # connect i to j
         connect_to = [nodes[j] for j in neigh_i]
@@ -165,18 +154,17 @@ if __name__ == '__main__':
         log.debug('node %s connected to %s', nodes[i], str(connect_to))
 
     # propose new transactions
-    for i in range(num_transactions):
+    for i in range(NUM_TXS):
         nodes[0].propose()
-        
-    for i in range(num_nodes):    
+
+    for i in range(NUM_NODES):
         nodes[i].run()
 
-    for i in range(num_nodes):
+    for i in range(NUM_NODES):
         s = nodes[i].storage
-        log.debug('node %s storage %s', i, s) 
+        log.debug('node %s storage %s', i, s)
 
-
-    #nodes[0].propose()
-    #for i in range(num_nodes-1):
+    # nodes[0].propose()
+    # for i in range(num_nodes-1):
     #    j = i + 1
     #    assert(nodes[i].storage == nodes[j].storage)
